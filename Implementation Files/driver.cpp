a610c9d4f5679f46e5a15f121430847e5b21dfa7
Shaun-MC <shaunmcushman@gmail.com>
2024-02-20 17:20:21 -0800

    imported files
    1st iteration of design diagram
    Input & sample output files
    created 1st iteration of the driver for the program

---
+++ Implementation Files/driver.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    MovieManager inventory;
+
+    inventory.ReadCustomers();
+
+    inventory.ReadMovies();
+
+    inventory.ReadTransactions();
+
+    return 1;
+}
