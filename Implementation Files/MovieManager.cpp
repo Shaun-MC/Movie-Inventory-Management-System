15e5157a5081a77e793c491bf8c9342db6b6c3ec
Shaun-MC <shaunmcushman@gmail.com>
2024-02-26 16:13:05 -0800

    ...

---
+++ Implementation Files/MovieManager.cpp
@@ -0,0 +1,11 @@
+#include "MovieManager.h"
+
+MovieManager::MovieManager() { // Unsure if nessecary - data types already initialize themselves
+
+
+}
+
+void MovieManager::ReadCustomers(ifstream& ifile) {
+
+    
+}
